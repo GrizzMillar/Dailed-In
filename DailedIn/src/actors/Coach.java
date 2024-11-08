8d7f4db083d2bf6c495a26a70fd47bd5f2cdebe0
Luke Millar <clukemillar@icloud.com>
2024-11-08 20:00:50 +0000

    Addition of Golfer, Coach, Round, Club classes

--- DailedIn/src/actors/Coach.java
+++ DailedIn/src/actors/Coach.java
@@ -1,5 +1,56 @@
 package actors;
 
+import java.util.LinkedList;
+
 public class Coach {
+	
+	private int userID;
+	private String email;
+	private String firstName;
+	private String secondName;
+	private String club;
+	private String status;
+	private LinkedList<Golfer> students;
+	
+	public Coach(int userID, String email, String firstName, String secondName, String club, String status) {
+		this.userID = userID;
+		this.email = email;
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.club = club;
+		this.status = status;
+	}
+	
+	public int getUserID() {
+		return this.userID;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getSecondName() {
+		return this.secondName;
+	}
+	
+	public String getClub() {
+		return this.club;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public LinkedList<Golfer> getStudents() {
+		return this.students;
+	}
+	
+	public void addStudent(Golfer golfer) {
+		this.students.add(golfer);
+	}
 
 }
--- DailedIn/src/actors/Golfer.java
+++ DailedIn/src/actors/Golfer.java
@@ -1,5 +1,64 @@
 package actors;
 
+import java.util.LinkedList;
+
+import round.Round;
+
 public class Golfer {
+	
+	private int userID;
+	private String email;
+	private String firstName;
+	private String secondName;
+	private double handicap;
+	private String club;
+	private LinkedList<Round> rounds;
+	private LinkedList<Club> bag;
+	
+	
+	public Golfer (int userID, String email, String firstName, String secondName, double handicap, String club) {
+		this.userID = userID;
+		this.email = email;
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.handicap = handicap;
+		this.club = club;
+	}
+	
+	public int getUserID() {
+		return this.userID;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getSecondName() {
+		return this.secondName;
+	}
+	
+	public Double getHandicap() {
+		return this.handicap;
+	}
+	
+	public String getClub() {
+		return this.club;
+	}
+	
+	public LinkedList<Round> getRounds() {
+		return this.rounds;
+	}
+	
+	public void addRound(Round round) {
+		this.rounds.add(round);
+	}
+	
+	public LinkedList<Club> getClubs() {
+		return this.bag;
+	}
+	
+	public void addClub(Club club) {
+		this.bag.add(club);
+	}
 
 }
